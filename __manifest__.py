7c36d599c31ab51ab3ce08c80690d19891cd42ee
Fabricio Costa Alisedo <fabricio.costa@moldeointeractive.com.ar>
2024-04-08 14:52:36 +0200

    Upgraded minor fix and cleaning logs 24.18

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
     'summary': 'MercadoLibre Publisher / Mercado Libre Odoo Connector',
-    'version': '15.0.24.17', 
+    'version': '15.0.24.18', 
     'author': 'Moldeo Interactive',
     'website': 'https://www.moldeointeractive.com',
     "category": "Sales",
